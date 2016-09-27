37b15289fb3a0a5288020dc4466aa7139230f989
gammaker <vav-94@mail.ru>
2016-09-27 22:39:28 +0300

    Music Synth warning fix

--- MusicSynthesizer/MusicSynthesizer.cpp
+++ MusicSynthesizer/MusicSynthesizer.cpp
@@ -11,6 +11,10 @@
 #include "Threading/Thread.h"
 
 #if(INTRA_PLATFORM_OS==INTRA_PLATFORM_OS_Windows)
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning(disable: 4668)
+#endif
 #ifndef WIN32_MEAN_AND_LEAN
 #define WIN32_MEAN_AND_LEAN
 #include <windows.h>
